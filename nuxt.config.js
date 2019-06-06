a0d46944083f315b087e678cd974fbc9437d7b39
Antonio <antonio@auro-group.com>
2019-06-06 19:35:09 +0200

    New title tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,11 +9,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Auro Travel | Auro New Transport Concept',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: "Auro Travel, la manera más cómoda de viajar. Embárcate en la flota líder" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
